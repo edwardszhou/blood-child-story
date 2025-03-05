567a3882f2e3a7655518a9f0dcda7d339ea9e56b
Edward Zhou <esz7923@nyu.edu>
2025-03-05 09:14:10 -0500

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      '@': path.resolve(__dirname, './src')
     }
   },
-  plugins: [react(), eslint(), tailwindcss()]
+  plugins: [react(), eslint(), tailwindcss()],
+  base: '/blood-child-story'
 });
